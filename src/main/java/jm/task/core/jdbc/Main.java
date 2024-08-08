168f5e29182714e3bc9bb0e543ebfb64925a96ff
Maximus972 <ManatilovgG@yandex.ru>
2024-08-08 13:49:01 +0300

    Работа с БД при помощи JDBC.

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -3,7 +3,6 @@ package jm.task.core.jdbc;
 import jm.task.core.jdbc.model.User;
 import jm.task.core.jdbc.service.UserServiceImpl;
 
-import java.sql.*;
 import java.util.List;
 
 public class Main {
