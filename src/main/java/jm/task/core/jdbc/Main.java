dfca19f4bef31e32be24bf5bad7020d9f482768c
Maximus972 <ManatilovgG@yandex.ru>
2024-08-08 13:48:34 +0300

    Работа с БД при помощи JDBC.

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -7,12 +7,8 @@ import java.sql.*;
 import java.util.List;
 
 public class Main {
-    private static final String URL = "jdbc:mysql://localhost:3306/sys";
-    private static final String USER = "root";
-    private static final String PASSWORD = "admin";
-    private static final String INSERT_NEW = "insert into sys values(?,?,?,?)";
 
-    public static void main(String[] args) throws SQLException {
+    public static void main(String[] args) {
         UserServiceImpl userService = new UserServiceImpl();
         userService.createUsersTable();
         userService.saveUser("John", "Travolta", (byte) 5);
@@ -20,7 +16,7 @@ public class Main {
         userService.saveUser("John", "Conor", (byte) 32);
         userService.saveUser("Jane", "Bob", (byte) 45);
         List<User> users = userService.getAllUsers();
-        users.stream().forEach(t -> System.out.println(t.toString()));
+        users.forEach(t -> System.out.println(t.toString()));
         userService.dropUsersTable();
     }
 }
